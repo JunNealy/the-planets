9117463c404118849b90db6a4c0bf810de15deb6
TNealy <66037350+JunNealy@users.noreply.github.com>
2023-12-03 16:15:26 -0500

    Updated git ignore,

---
+++ src/data.js
@@ -0,0 +1,14 @@
+export const contentMap = {
+  earth: {
+    name: 'earth',
+    overview:
+      "Third planet from the Sun and the only known planet to harbor life. About 29.2% of Earth's surface is land with remaining 70.8% is covered with water. Earth's distance from the Sun, physical properties  and geological history have allowed life to evolve and thrive.",
+    source: 'https://en.wikipedia.org/wiki/Earth ',
+  },
+  mars: {
+    name: 'MARS',
+    overview:
+      "Third planet from the Sun and the only known planet to harbor life. About 29.2% of Earth's surface is land with remaining 70.8% is covered with water. Earth's distance from the Sun, physical properties  and geological history have allowed life to evolve and thrive.",
+    source: 'https://en.wikipedia.org/wiki/Earth ',
+  },
+};
