e14b39927825318fac2ceef4aca6bf7909094d78
TNealy <66037350+JunNealy@users.noreply.github.com>
2023-12-14 13:56:45 -0500

    Mobile nav burger icon functionality

--- src/components/mobileNav.jsx
+++ src/components/mobileNav.jsx
@@ -1,23 +1,34 @@
 import { planetInfo } from '../data.js';
+import '../styles/styles.css';
+
+const navButton = document.getElementById('nav-button');
+
+const mobileNav = document.getElementById('mobile-nav-container');
+
+navButton.addEventListener('click', () => {
+  if (mobileNav.style.display === 'block') {
+    mobileNav.style.display = 'none';
+  } else {
+    mobileNav.style.display = 'block';
+  }
+});
 
 function MobileNav() {
   const listItmes = planetInfo.map((planet) => (
-    <li key={planet.id}>
+    <li id="mobile-nav-item" key={planet.id}>
       <div
+        id="mobile-nav-item-decoration"
         style={{
-          borderRadius: '50000px',
-          height: '10px',
-          width: '10px',
           backgroundColor: `${planet.keyColor}`,
         }}
       ></div>
-      {planet.name}
+      {planet.name.toUpperCase()}
     </li>
   ));
 
   return (
-    <div>
-      <ul>{listItmes}</ul>
+    <div id="mobile-nav-container">
+      <ul id="mobile-nav-list">{listItmes}</ul>
     </div>
   );
 }
