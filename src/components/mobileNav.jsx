84ba64d43d9f408c0d355646d55ecd50a3b903c9
TNealy <66037350+JunNealy@users.noreply.github.com>
2023-12-14 16:50:20 -0500

    Menu Toggle Functionality

--- src/components/mobileNav.jsx
+++ src/components/mobileNav.jsx
@@ -1,34 +1,44 @@
 import { planetInfo } from '../data.js';
 import '../styles/styles.css';
-
-const navButton = document.getElementById('nav-button');
-
-const mobileNav = document.getElementById('mobile-nav-container');
-
-navButton.addEventListener('click', () => {
-  if (mobileNav.style.display === 'block') {
-    mobileNav.style.display = 'none';
-  } else {
-    mobileNav.style.display = 'block';
-  }
-});
+import { useEffect, useState } from 'react';
 
 function MobileNav() {
-  const listItmes = planetInfo.map((planet) => (
-    <li id="mobile-nav-item" key={planet.id}>
+  const [displayStyle, setDisplayStyle] = useState('none');
+
+  useEffect(() => {
+    const navButton = document.getElementById('nav-button');
+
+    const toggleMobileNav = () => {
+      setDisplayStyle((prevStyle) =>
+        prevStyle === 'block' ? 'none' : 'block'
+      );
+    };
+    if (navButton) {
+      navButton.addEventListener('click', toggleMobileNav);
+    }
+
+    return () => {
+      if (navButton) {
+        navButton.removeEventListener('click', toggleMobileNav);
+      }
+    };
+  }, []);
+
+  const listItems = planetInfo.map((planet) => (
+    <li key={planet.id} id="mobile-nav-item">
       <div
         id="mobile-nav-item-decoration"
         style={{
           backgroundColor: `${planet.keyColor}`,
         }}
       ></div>
-      {planet.name.toUpperCase()}
+      <span id="mobile-nav-item-name">{planet.name.toUpperCase()}</span>
     </li>
   ));
 
   return (
-    <div id="mobile-nav-container">
-      <ul id="mobile-nav-list">{listItmes}</ul>
+    <div id="mobile-nav-container" style={{ display: displayStyle }}>
+      <ul id="mobile-nav-list">{listItems}</ul>
     </div>
   );
 }
