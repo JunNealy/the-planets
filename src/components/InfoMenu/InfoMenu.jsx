7e6e7b51f60ce3d984f21c9deca72e2a7605c4f6
TNealy <66037350+JunNealy@users.noreply.github.com>
2024-01-08 16:22:02 -0500

    Tablet breakpoint Styling

--- src/components/InfoMenu/InfoMenu.jsx
+++ src/components/InfoMenu/InfoMenu.jsx
@@ -5,57 +5,78 @@ import './InfoMenu.css';
 const InfoMenu = ({ onDetailsChange, selectedPlanet }) => {
   const [selectedHeading, setSelectedHeading] = useState('overview');
 
-  console.log(selectedPlanet);
+  const [selectedButton, setSelectedButton] = useState('overview');
+
+  const handleButtonClick = (buttonName) => {
+    setSelectedButton(buttonName);
+    onDetailsChange(buttonName);
+  };
 
   useEffect(() => {
-    headingHighlight();
-  }, [selectedHeading]);
+    const buttonHighlight = () => {
+      const getPlanetKeyColor = () => {
+        const planet = filterPlanets(planetInfo, selectedPlanet);
+        console.log(planet.keyColor);
+        return planet.keyColor;
+      };
 
-  const headingHighlight = () => {
-    const headings = document.querySelectorAll('.mobile-info-button');
-    //style reset
-    headings.forEach((heading) => {
-      heading.style.borderBottom = '3px solid transparent';
-      heading.style.color = '#838391';
-      console.log('style reset');
-    });
-    //restyle on select
-    const selectedHeadingElement = document.getElementById(selectedHeading);
-    if (selectedHeadingElement) {
-      selectedHeadingElement.style.borderBottom = `3px solid ${getPlanetKeyColor()}`;
-      selectedHeadingElement.style.color = 'white';
-    }
-  };
+      function filterPlanets(arr, selPlanet) {
+        for (let i = 0; i < arr.length; i++) {
+          if (arr[i].name.includes(selPlanet)) {
+            return arr[i];
+          }
+        }
+      }
+      const buttons = document.querySelectorAll('.info-menu-button');
+      //style reset
+      buttons.forEach((button) => {
+        button.style.backgroundColor = '';
+        button.style.color = 'white';
+        console.log('style reset');
+      });
+      //restyle on select
+      const selectedButtonElement = document.getElementById(selectedButton);
+      if (selectedButtonElement) {
+        selectedButtonElement.style.backgroundColor = `${getPlanetKeyColor()}`;
+      }
+    };
+    buttonHighlight();
+  }, [selectedButton]);
 
   const handleHeadingClick = (headingName) => {
     setSelectedHeading(headingName);
     onDetailsChange(headingName);
   };
 
-  const getPlanetKeyColor = () => {
-    const planet = filterPlanets(planetInfo, selectedPlanet);
-    return planet.keyColor;
-  };
-
-  function filterPlanets(arr, selPlanet) {
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i].name.includes(selPlanet)) {
-        return arr[i];
-      }
-    }
-  }
-
   return (
     <div id="info-menu">
-      <button className="info-menu-button">
+      <button
+        className="info-menu-button"
+        id="overview"
+        onClick={() => {
+          handleButtonClick('overview');
+        }}
+      >
         <p className="button-count">01</p>
         <p className="button-name">OVERVIEW</p>
       </button>
-      <button className="info-menu-button">
+      <button
+        className="info-menu-button"
+        id="internalStruct"
+        onClick={() => {
+          handleButtonClick('internalStruct');
+        }}
+      >
         <p className="button-count">02</p>
         <p className="button-name">INTERNAL STRUCTURE</p>
       </button>
-      <button className="info-menu-button">
+      <button
+        className="info-menu-button"
+        id="surfaceGeo"
+        onClick={() => {
+          handleButtonClick('surfaceGeo');
+        }}
+      >
         <p className="button-count">03</p>
         <p className="button-name">SURFACE GEOLOGY</p>
       </button>
