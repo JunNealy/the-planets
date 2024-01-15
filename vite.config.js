29987cfd235812fa16ec925246ee95e515c260f2
TNealy <66037350+JunNealy@users.noreply.github.com>
2024-01-15 12:20:21 -0500

    gh-pages config - dynamic images base url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/the-planets',
+  base: '/the-planets/',
   plugins: [react()],
 });
